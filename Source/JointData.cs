7246a5e7922502374a4a573fd6f896cda9de2431
unknown <dmanning23@hotmail.com>
2013-10-05 01:06:13 -0500

    joint unit tests

--- Source/JointData.cs
+++ Source/JointData.cs
@@ -16,13 +16,15 @@ namespace AnimationLib
 
 		/// <summary>
 		/// the first rotation limit for ragdoll
+		/// user -180 and 180 as default limits
 		/// </summary>
-		private float m_FirstLimit;
+		private float m_FirstLimit = -MathHelper.Pi;
 
 		/// <summary>
 		/// the second rotation limit for ragdoll
+		/// user -180 and 180 as default limits
 		/// </summary>
-		private float m_SecondLimit;
+		private float m_SecondLimit = MathHelper.Pi;
 
 		/// <summary>
 		/// the first rotation limit for ragdoll when parent bone is flipped
@@ -33,23 +35,6 @@ namespace AnimationLib
 		/// the second rotation limit for ragdoll when parent bone is flipped
 		/// </summary>
 		private float m_SecondLimitFlipped;
-
-		/// <summary>
-		/// The radius of the circle that ragdoll is allowed to float around
-		/// </summary>
-		private float m_fFloatRadius;
-
-		/// <summary>
-		/// Whether this joint will use floating or rotating ragdoll
-		/// Float uses the ragdollradius to float around the anchor joint
-		/// chained uses the limits to rotate around the anchor joint
-		/// </summary>
-		private bool m_bFloatRagdoll;
-
-		/// <summary>
-		/// The distance from teh anchor position to this joint position
-		/// </summary>
-		private float m_fLength;
 
 		/// <summary>
 		/// This is the vector from this joint position to the anchor location
@@ -69,30 +54,27 @@ namespace AnimationLib
 			set { m_Location = value; }
 		}
 
-		public float Length
-		{
-			get { return m_fLength; }
-		}
+		/// <summary>
+		/// The distance from teh anchor position to this joint position
+		/// </summary>
+		public float Length { get; private set; }
 
 		public Vector2 AnchorVect
 		{
 			get { return m_AnchorVect; }
 		}
 
-		public bool Floating
-		{
-			get { return m_bFloatRagdoll; }
-			set { m_bFloatRagdoll = value; }
-		}
+		/// <summary>
+		/// Whether this joint will use floating or rotating ragdoll
+		/// Float uses the ragdollradius to float around the anchor joint
+		/// chained uses the limits to rotate around the anchor joint
+		/// </summary>
+		public bool Floating { get; set; }
 
-		public float FloatRadius
-		{
-			get { return m_fFloatRadius; }
-			set 
-			{ 
-				m_fFloatRadius = value; 
-			}
-		}
+		/// <summary>
+		/// The radius of the circle that ragdoll is allowed to float around
+		/// </summary>
+		public float FloatRadius { get; set; }
 
 		public float FirstLimit
 		{
@@ -114,15 +96,9 @@ namespace AnimationLib
 			}
 		}
 
-		public float FirstLimitFlipped
-		{
-			get { return m_FirstLimitFlipped; }
-		}
+		public float FirstLimitFlipped { get; private set; }
 
-		public float SecondLimitFlipped
-		{
-			get { return m_SecondLimitFlipped; }
-		}
+		public float SecondLimitFlipped { get; private set; }
 
 		#endregion
 
@@ -132,14 +108,12 @@ namespace AnimationLib
 		{
 			m_Location = new Vector2(0.0f);
 			m_AnchorVect = new Vector2(0.0f);
-			m_fLength = 0.0f;
+			Length = 0.0f;
 
-			//user -180 and 180 as default limits
-			FirstLimit = -MathHelper.Pi;
-			SecondLimit = MathHelper.Pi;
 
-			m_fFloatRadius = 0.0f;
-			m_bFloatRagdoll = false;
+
+			FloatRadius = 0.0f;
+			Floating = false;
 		}
 
 		public void Copy(JointData myInst)
@@ -147,9 +121,9 @@ namespace AnimationLib
 			m_Location = myInst.m_Location;
 			FirstLimit = myInst.FirstLimit;
 			SecondLimit = myInst.SecondLimit;
-			m_fFloatRadius = myInst.FloatRadius;
-			m_bFloatRagdoll = myInst.m_bFloatRagdoll;
-			m_fLength = myInst.m_fLength;
+			FloatRadius = myInst.FloatRadius;
+			Floating = myInst.Floating;
+			Length = myInst.Length;
 			m_AnchorVect = myInst.m_AnchorVect;
 		}
 
@@ -159,8 +133,8 @@ namespace AnimationLib
 		private void UpdateFlippedLimits()
 		{
 			//Subtract the limits from 180 and switch them
-			m_SecondLimitFlipped = -1.0f * m_FirstLimit;
-			m_FirstLimitFlipped = -1.0f * m_SecondLimit;
+			SecondLimitFlipped = -1.0f * FirstLimit;
+			FirstLimitFlipped = -1.0f * SecondLimit;
 		}
 
 		#endregion //Methods
@@ -218,22 +192,22 @@ namespace AnimationLib
 					else if (strName == "limit2")
 					{
 						float fMyLimit = Convert.ToSingle(strValue);
-						m_SecondLimit = MathHelper.ToRadians(fMyLimit);
+						SecondLimit = MathHelper.ToRadians(fMyLimit);
 					}
 					else if (strName == "FloatRadius")
 					{
-						m_fFloatRadius = Convert.ToSingle(strValue);
+						FloatRadius = Convert.ToSingle(strValue);
 					}
 					else if (strName == "FloatOrRotate")
 					{
-						m_bFloatRagdoll = Convert.ToBoolean(strValue);
+						Floating = Convert.ToBoolean(strValue);
 					}
 				}
 			}
 
 			//get the vector from the anchor position to this joint position
 			m_AnchorVect = Location - myImage.AnchorCoord;
-			m_fLength = m_AnchorVect.Length();
+			Length = m_AnchorVect.Length();
 
 			return true;
 		}
@@ -256,13 +230,13 @@ namespace AnimationLib
 
 			//write first limit 
 			rXMLFile.WriteStartElement("limit1");
-			float fLimit1 = Helper.ClampAngle(m_FirstLimit);
+			float fLimit1 = Helper.ClampAngle(FirstLimit);
 			rXMLFile.WriteString(MathHelper.ToDegrees(fLimit1).ToString());
 			rXMLFile.WriteEndElement();
 
 			//write 2nd limit 
 			rXMLFile.WriteStartElement("limit2");
-			float fLimit2 = Helper.ClampAngle(m_SecondLimit);
+			float fLimit2 = Helper.ClampAngle(SecondLimit);
 			rXMLFile.WriteString(MathHelper.ToDegrees(fLimit2).ToString());
 			rXMLFile.WriteEndElement();
 
@@ -273,7 +247,7 @@ namespace AnimationLib
 
 			//write out whether it uses float or rotate ragdoll
 			rXMLFile.WriteStartElement("FloatOrRotate");
-			rXMLFile.WriteString(m_bFloatRagdoll ? "true" : "false");
+			rXMLFile.WriteString(Floating ? "true" : "false");
 			rXMLFile.WriteEndElement();
 
 			rXMLFile.WriteEndElement();
@@ -288,12 +262,12 @@ namespace AnimationLib
 			Location = rData.location;
 			FirstLimit = MathHelper.ToRadians(rData.limit1);
 			SecondLimit = MathHelper.ToRadians(rData.limit2);
-			m_fFloatRadius = rData.FloatRadius;
-			m_bFloatRagdoll = rData.FloatOrRotate;
+			FloatRadius = rData.FloatRadius;
+			Floating = rData.FloatOrRotate;
 
 			//get the vector from the anchor position to this joint position
 			m_AnchorVect = Location - myImage.AnchorCoord;
-			m_fLength = m_AnchorVect.Length();
+			Length = m_AnchorVect.Length();
 			return true;
 		}
 
--- Tests/JointDataTests.cs
+++ Tests/JointDataTests.cs
@@ -1,14 +1,78 @@
 using NUnit.Framework;
 using System;
+using AnimationLib;
+using FilenameBuddy;
+using RenderBuddy.Tests;
+using RenderBuddy;
+using Microsoft.Xna.Framework;
 
 namespace Animationlib.Tests
 {
 	[TestFixture()]
 	public class JointDataTests
 	{
-		[Test()]
-		public void TestCase()
+		Image image;
+
+		[SetUp()]
+		public void Setup()
 		{
+			AnimationContainer test = new AnimationContainer();
+			Filename testFile = new Filename();
+			testFile.SetRelFilename("SuperSimple\\Simple Model.xml");
+			TestRenderer renderer = new TestRenderer();
+			bool bTest = test.ReadXMLModelFormat(testFile.File, renderer);
+			Bone crotch = test.Model.GetBone("Crotch");
+			Filename file = new Filename();
+			file.SetRelFilename(@"SuperSimple\CrotchFront.png");
+			image = crotch.GetImage(file);
+		}
+
+		[Test()]
+		public void Loaded()
+		{
+			Assert.AreEqual(1, image.JointCoords.Count);
+		}
+
+		[Test()]
+		public void Location()
+		{
+			Assert.AreEqual(new Vector2(67.0f, 44.0f), image.JointCoords[0].Location);
+		}
+
+		[Test()]
+		public void Limit1()
+		{
+			Assert.AreEqual(-180.0f, MathHelper.ToDegrees(image.JointCoords[0].FirstLimit));
+		}
+
+		[Test()]
+		public void Limit2()
+		{
+			Assert.AreEqual(260.0f, MathHelper.ToDegrees(image.JointCoords[0].SecondLimit));
+		}
+
+		[Test()]
+		public void Limit1Flip()
+		{
+			Assert.AreEqual(-260.0f, MathHelper.ToDegrees(image.JointCoords[0].FirstLimitFlipped));
+		}
+
+		[Test()]
+		public void Limit2Flip()
+		{
+			Assert.AreEqual(180.0f, MathHelper.ToDegrees(image.JointCoords[0].SecondLimitFlipped));
+		}
+
+		[Test()]
+		public void Floated()
+		{
+			Assert.IsTrue(image.JointCoords[0].Floating);
+		}
+
+		[Test()]
+		public void Length()
+		{
+			Assert.AreEqual(10.0f, image.JointCoords[0].FloatRadius);
 		}
 	}
 }
