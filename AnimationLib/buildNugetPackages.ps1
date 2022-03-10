12913db0de6f5bbd0d8914af2e75824f1a8d3f02
Dan Manning <dmanning@oceaneering.com>
2022-03-10 15:34:43 -0600

    removed bridge project

--- AnimationLib/buildNugetPackages.ps1
+++ AnimationLib/buildNugetPackages.ps1
@@ -1,4 +1,3 @@
 rm *.nupkg
 nuget pack .\AnimationLib.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\AnimationLib.Bridge.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 cp *.nupkg C:\Projects\Nugets\
